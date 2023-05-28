c3541450b489eda4d8df35b791c96afbc6cd2d6d
Tiemen Schut <tschut@gmail.com>
2023-05-28 21:55:51 +0200

    Solution for 5-2

--- src/main/kotlin/nl/tiemenschut/aoc/y2015/day5.kt
+++ src/main/kotlin/nl/tiemenschut/aoc/y2015/day5.kt
@@ -12,24 +12,40 @@ fun main() {
         val illegals = listOf("ab", "cd", "pq", "xy")
         fun Char.isVowel() = this in vowels
 
-        fun String.isNice(): Boolean {
-            var vowelCount = 0
-            var hasRepeatingPattern = false
+        part1 { input ->
+            fun String.isNice(): Boolean {
+                var vowelCount = 0
+                var hasRepeatingPattern = false
 
-            this.windowedSequence(2, 1, partialWindows = true).forEach { window ->
-                if (window in illegals) return false
-                if (!hasRepeatingPattern && window.length == 2 && window[0] == window[1]) hasRepeatingPattern = true
-                if (window.first().isVowel()) vowelCount++
+                this.windowedSequence(2, 1, partialWindows = true).forEach { window ->
+                    if (window in illegals) return false
+                    if (!hasRepeatingPattern && window.length == 2 && window[0] == window[1]) hasRepeatingPattern = true
+                    if (window.first().isVowel()) vowelCount++
+                }
+
+                return vowelCount >= 3 && hasRepeatingPattern
             }
 
-            return vowelCount >= 3 && hasRepeatingPattern
-        }
-
-        part1 { input ->
             input.count { it.isNice() }
         }
 
-//        part2 { input ->
-//        }
+        part2 { input ->
+            fun String.isNice(): Boolean {
+                var condition1 = false
+                for (i in 0 until this.length - 1) {
+                    val pair = "${this[i]}${this[i+1]}"
+                    if (pair in this.substring(i + 2)) {
+                        condition1 = true
+                        break
+                    }
+                }
+
+                val condition2 = this.windowedSequence(3, 1).any { it[0] == it[2] }
+
+                return condition1 && condition2
+            }
+
+            input.count { it.isNice() }
+        }
     }
 }
